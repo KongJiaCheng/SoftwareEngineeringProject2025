6b3e877ea1452ce384beaf4c4ee374c549aed364
KongJiaCheng <robotaliceskull@gmail.com>
2025-10-14 08:02:51 +0800

    Delete providers.js

--- providers.js
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-
-export function Providers({ children }) {
-  return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>;
-}
