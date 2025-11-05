759194fe9e39b6191287d141d99da26ddd99a740
Zuerrr <zuerkoed0927@gmail.com>
2025-11-05 14:05:47 +0800

    sync with origin repo

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: B:\degree\miantic\SoftwareEngineeringProject2025\frontend\app\layout.js
+// File: C:\xampp\htdocs\SoftwareEngineeringProject2025\SoftwareEngineeringProject2025\frontend\app\layout.js
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
