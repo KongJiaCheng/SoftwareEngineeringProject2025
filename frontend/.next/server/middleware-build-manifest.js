4b84acd7a45d0164af0f88980e3c5bb8ed97ca9a
jeremy-p25017060 <p25017060@student.newinti.edu.my>
2025-11-12 23:46:40 +0800

    commit

--- frontend/.next/server/middleware-build-manifest.js
+++ frontend/.next/server/middleware-build-manifest.js
@@ -2,13 +2,7 @@ globalThis.__BUILD_MANIFEST = {
   "polyfillFiles": [
     "static/chunks/polyfills.js"
   ],
-<<<<<<< HEAD
-  "devFiles": [
-    "static/chunks/react-refresh.js"
-  ],
-=======
   "devFiles": [],
->>>>>>> origin/main
   "ampDevFiles": [],
   "lowPriorityFiles": [],
   "rootMainFiles": [
@@ -17,20 +11,7 @@ globalThis.__BUILD_MANIFEST = {
   ],
   "rootMainFilesTree": {},
   "pages": {
-<<<<<<< HEAD
-    "/_app": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_app.js"
-    ],
-    "/_error": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/_error.js"
-    ]
-=======
     "/_app": []
->>>>>>> origin/main
   },
   "ampFirstPages": []
 };
